46d278eaa798eb973892b05eec080c64080c1b9e
AkashSatla <satlaakash@gmail.com>
2022-03-11 20:32:29 +0530

    ADDED: A compareTo method to compare lines

--- src/main/Line.java
+++ src/main/Line.java
@@ -5,12 +5,12 @@ public class Line {
         return Math.pow(Math.pow(x2-x1,2)+Math.pow(y2-y1,2),0.5);
     }
 
-    public void equalLines(Double length1,Double length2) {
-        boolean equality = length1.equals(length2);
-
-        if (equality)
-            System.out.println("Both lines are equal");
-        else
-            System.out.println("Lines are not equal");
+    public void compareLines(Double length3, Double length4){
+        if (length3.compareTo(length4)==0)
+            System.out.println("lines are equal");
+        else if(length3.compareTo(length4)>0)
+            System.out.println("First line is larger than Second");
+        else if (length3.compareTo(length4)<0)
+            System.out.println("First line is smaller than Second");
     }
 }
--- src/main/LineTest.java
+++ src/main/LineTest.java
@@ -4,7 +4,7 @@ public class LineTest {
     public static void main(String[] args) {
         Line line = new Line();
         double lineLength1=line.lengthOfLine(1,2,10,11);
-        double lineLength2=line.lengthOfLine(3,4,12,13);
-        line.equalLines(lineLength1,lineLength2);
+        double lineLength2=line.lengthOfLine(3,4,12,14);
+        line.compareLines(lineLength1,lineLength2);
     }
 }
