d4f93012a93c5d8092bd0e5ff1709694678dd0ce
AkashSatla <satlaakash@gmail.com>
2022-03-11 20:17:59 +0530

    ADDED: A method to calculate Line length

---
+++ src/main/Line.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Line {
+    public double lengthOfLine(double x1, double y1, double x2, double y2){
+        return Math.pow(Math.pow(x2-x1,2)+Math.pow(y2-y1,2),0.5);
+    }
+}
---
+++ src/main/LineTest.java
@@ -0,0 +1,9 @@
+package main;
+
+public class LineTest {
+    public static void main(String[] args) {
+        Line line = new Line();
+        double lineLength=line.lengthOfLine(1,2,10,11);
+        System.out.println("Length of the line l1 is "+lineLength);
+    }
+}
