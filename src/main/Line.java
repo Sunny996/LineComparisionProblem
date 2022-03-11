cf0f5c153a1ec033c8ae803bf1b6892ba3dff369
AkashSatla <satlaakash@gmail.com>
2022-03-11 20:24:09 +0530

    ADDED: A method to calculate the equality of lines

--- src/main/Line.java
+++ src/main/Line.java
@@ -4,4 +4,13 @@ public class Line {
     public double lengthOfLine(double x1, double y1, double x2, double y2){
         return Math.pow(Math.pow(x2-x1,2)+Math.pow(y2-y1,2),0.5);
     }
+
+    public void equalLines(Double length1,Double length2) {
+        boolean equality = length1.equals(length2);
+
+        if (equality)
+            System.out.println("Both lines are equal");
+        else
+            System.out.println("Lines are not equal");
+    }
 }
--- src/main/LineTest.java
+++ src/main/LineTest.java
@@ -3,7 +3,8 @@ package main;
 public class LineTest {
     public static void main(String[] args) {
         Line line = new Line();
-        double lineLength=line.lengthOfLine(1,2,10,11);
-        System.out.println("Length of the line l1 is "+lineLength);
+        double lineLength1=line.lengthOfLine(1,2,10,11);
+        double lineLength2=line.lengthOfLine(3,4,12,13);
+        line.equalLines(lineLength1,lineLength2);
     }
 }
